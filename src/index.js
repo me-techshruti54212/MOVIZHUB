bda2f2ab9895dd6c64e3703151948d24fc4e4b65
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-06-29 19:41:02 +0530

    routing

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter basename="/MovizHub">
+  <BrowserRouter>
     <App/>
     </BrowserRouter>
   // </React.StrictMode>
