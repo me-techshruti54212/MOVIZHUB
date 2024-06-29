16db08a43248eb6cf8b5980f12fb7bd2e646c255
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-06-29 17:42:40 +0530

    basename added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
+  <BrowserRouter basename="/MovizHub">
     <App/>
+    </BrowserRouter>
   // </React.StrictMode>
 );
 
